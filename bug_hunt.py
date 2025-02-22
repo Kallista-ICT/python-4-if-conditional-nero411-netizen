1714eab124b64538a9cc45680bf764988437fe77
nero411-netizen <albertnero32@gmail.com>
2025-02-22 15:04:58 +0000

    Submit python 4

--- bug_hunt.py
+++ bug_hunt.py
@@ -1,14 +1,14 @@
 # Get the grade from the user as input  
-user_grade = input("Enter your grade: ")   
+user_grade = int(input("Enter your grade: ")) 
 
 # Determine the grade symbol based on the user's input  
-if user_grade >= 90:  # Check if the grade is 90 or above  
+if user_grade < 100:  # Check if the grade is 90 or above  
     category_name = "A"  
-elif user_grade >= 80:  # Check if the grade is 80 or above  
+elif 80 <= user_grade <= 90:  # Check if the grade is 80 or above  
     category_name = "B"  
-elif user_grade >= 70:  # Check if the grade is 70 or above  
+elif user_grade <= 70:  # Check if the grade is 70 or above  
     category_name = "C"  
-elif user_grade >= 60:  # Check if the grade is 60 or above  
+elif user_grade <= 60:  # Check if the grade is 60 or above  
     category_name = "D"  
 else:  # If none of the above, the grade is below 60  
     category_name = "F"  
